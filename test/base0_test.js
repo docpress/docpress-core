da1f623179f99ac7cb094c3747a03a7c777bcfb2
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2015-10-03 23:32:24 +0800

    Test base0

---
+++ test/base0_test.js
@@ -0,0 +1,59 @@
+const base0 = require('../lib/base0')
+
+describe('base0', function () {
+  beforeEach(function (done) {
+    var ms = {}
+    this.files = {
+      'docs/README.md': {
+        contents: [
+          '# toc',
+          '',
+          '* [Readme](/README.md)',
+          '* [Getting started](/docs/getting-started.md)'
+        ].join('\n') + '\n'
+      },
+      'docs/getting-started.md': {
+        contents: '# getting started\n'
+      },
+      'README.md': {
+        contents: '# hello\n'
+      }
+    }
+
+    base0(this.files, ms, (err) => {
+      if (err) throw err
+      done()
+    })
+  })
+
+  it('renders index.html', function () {
+    const idx = this.files['index.html']
+    expect(idx).toBeAn('object')
+    expect(idx.title).toEqual('Readme')
+    expect(idx.contents).toEqual('<h1 id="hello">hello</h1>\n')
+    expect(idx.markdown).toBeA('string')
+    expect(idx.source).toEqual('README.md')
+  })
+
+  describe('toc.json:', function () {
+    beforeEach(function () {
+      this.tocFile = this.files['toc.json']
+      this.toc = JSON.parse(this.tocFile.contents)
+    })
+
+    it('renders.json', function () {
+      expect(this.toc.sections).toBeAn('array')
+      expect(this.toc.sections.length).toEqual(2)
+    })
+
+    it('renders the first section', function () {
+      const section = this.toc.sections[0]
+      expect(section).toBeAn('object')
+      expect(section).toEqual({
+        title: 'Readme',
+        source: 'README.md',
+        url: 'index.html'
+      })
+    })
+  })
+})
