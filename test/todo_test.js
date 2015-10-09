653e6887ffd310305f98e0859ecab8d614215fb7
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2015-10-10 02:33:54 +0800

    Update to do

--- test/todo_test.js
+++ test/todo_test.js
@@ -1,22 +1,30 @@
 describe('to do', function () {
-  it('parsing toc', done)
-  it('flattening toc to index', done)
-  it('rendering pages', done)
-  it('strip out docs/ prefix', done)
-  it('test fail cases', done)
-  it('prettifying pages', done)
-  it('handle relative urls', done)
-  it('validating toc', done)
-  it('allow http:// links')
-  it('transforming links in document')
-  it('index sub-toc', done)
-  it('single readme mode')
-  it('expand mode', done) /* make it bold in the TOC */
-  it('warn when no pages are parsed')
-  it('previous/next')
-  it('account for links in headings')
-  it('account for question marks in headings')
-  it('move loading plugins into docpress-core/ms')
+  describe('done', function () {
+    it('parsing toc', done)
+    it('flattening toc to index', done)
+    it('rendering pages', done)
+    it('strip out docs/ prefix', done)
+    it('test fail cases', done)
+    it('prettifying pages', done)
+    it('handle relative urls', done)
+    it('validating toc', done)
+    it('allow http:// links')
+    it('index sub-toc', done)
+    it('expand mode', done) /* make it bold in the TOC */
+  })
+
+  describe('to do', function () {
+    it('single readme mode')
+    it('warn when no pages are parsed')
+    it('account for links in headings')
+    it('account for question marks in headings')
+    it('move loading plugins into docpress-core/ms')
+  })
+
+  describe('transforming html', function () {
+    it('transforming links in document')
+    it('highlight it already')
+  })
 })
 
 function done () {}
