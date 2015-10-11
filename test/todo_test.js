c123696de83fe563f3a5edb9e1c2fdd79a2b23e1
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2015-10-12 00:47:56 +0800

    Update todo

--- test/todo_test.js
+++ test/todo_test.js
@@ -8,23 +8,27 @@ describe('to do', function () {
     it('prettifying pages', done)
     it('handle relative urls', done)
     it('validating toc', done)
-    it('allow http:// links')
     it('index sub-toc', done)
     it('expand mode', done) /* make it bold in the TOC */
   })
 
   describe('to do', function () {
+    it('bug: linking pages inside sub-directories')
+    it('bug: linking pages with absolute references')
+    it('bug: linking to #anchors')
     it('single readme mode')
     it('warn when no pages are parsed')
     it('account for links in headings')
     it('account for question marks in headings')
     it('move loading plugins into docpress-core/ms')
+    it('link urls (http://...)')
     it('delete docpress.json')
   })
 
   describe('transforming html', function () {
     it('transforming links in document', done)
     it('syntax highlighting', done)
+    it('set meta description')
   })
 })
 
