869566a7bc9ab80137b7f00bb0d26160d5f09981
chris <dwatow@gmail.com>
2019-11-21 10:37:04 +0800

    fix support make it backwards compatible

--- test/fixture/markdown_plugin_invalid_test.js
+++ test/fixture/markdown_plugin_invalid_test.js
@@ -3,21 +3,43 @@
 const fixture = require('../support/fixture')
 
 describe('fixture/markdown plugin invalid:', function () {
-  let app
+  describe('array config', function () {
+    let app
 
-  const fx = fixture('markdown-plugin-invalid')
+    const fx = fixture('markdown-plugin-invalid-array-config')
 
-  before(function (done) {
-    app = require(fx.path('metalsmith.js'))
-    app.build((err, files) => {
-      this.files = files
-      if (err) this.err = err
-      done()
+    before(function (done) {
+      app = require(fx.path('metalsmith.js'))
+      app.build((err, files) => {
+        this.files = files
+        if (err) this.err = err
+        done()
+      })
+    })
+
+    it('fails', function () {
+      expect(this.err).toBeDefined()
+      expect(this.err.message).toEqual("Can't find module 'markdown-it-lalalala'")
     })
   })
 
-  it('fails', function () {
-    expect(this.err).toBeDefined()
-    expect(this.err.message).toEqual("Can't find module 'markdown-it-lalalala'")
+  describe('object config', function () {
+    let app
+
+    const fx = fixture('markdown-plugin-invalid-object-config')
+
+    before(function (done) {
+      app = require(fx.path('metalsmith.js'))
+      app.build((err, files) => {
+        this.files = files
+        if (err) this.err = err
+        done()
+      })
+    })
+
+    it('fails', function () {
+      expect(this.err).toBeDefined()
+      expect(this.err.message).toEqual("Can't find module 'markdown-it-lalalala'")
+    })
   })
 })
--- test/fixture/markdown_plugin_test.js
+++ test/fixture/markdown_plugin_test.js
@@ -3,25 +3,51 @@
 const fixture = require('../support/fixture')
 
 describe('fixture/markdown plugin:', function () {
-  let app
+  describe('array config:', function () {
+    let app
 
-  const fx = fixture('markdown-plugin')
+    const fx = fixture('markdown-plugin-array-config')
 
-  before(function (done) {
-    app = require(fx.path('metalsmith.js'))
-    app.build((err, files) => {
-      this.files = files
-      if (err) return done(err)
-      done()
+    before(function (done) {
+      app = require(fx.path('metalsmith.js'))
+      app.build((err, files) => {
+        this.files = files
+        if (err) return done(err)
+        done()
+      })
+    })
+
+    it('outputs the right files', function () {
+      expect(fx.exists('_docpress/index.html')).toEqual(true)
+    })
+
+    it('makes markdown-it-decorate work', function () {
+      expect(fx.read('_docpress/index.html'))
+        .toContain('<h1 class="hello" id="my-project">')
     })
   })
 
-  it('outputs the right files', function () {
-    expect(fx.exists('_docpress/index.html')).toEqual(true)
-  })
+  describe('object config:', function () {
+    let app
 
-  it('makes markdown-it-decorate work', function () {
-    expect(fx.read('_docpress/index.html'))
-      .toContain('<h1 class="hello" id="my-project">')
+    const fx = fixture('markdown-plugin-object-config')
+
+    before(function (done) {
+      app = require(fx.path('metalsmith.js'))
+      app.build((err, files) => {
+        this.files = files
+        if (err) return done(err)
+        done()
+      })
+    })
+
+    it('outputs the right files', function () {
+      expect(fx.exists('_docpress/index.html')).toEqual(true)
+    })
+
+    it('makes markdown-it-decorate work', function () {
+      expect(fx.read('_docpress/index.html'))
+        .toContain('<h1 class="hello" id="my-project">')
+    })
   })
 })
